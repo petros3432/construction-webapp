38aab671b9c2d6f19455f76a38c63080bd87c2bf
Petros <petros.1822@gmail.com>
2020-12-03 13:33:28 +0200

    Initial commit

---
+++ src/main/java/gr/pf/team2/constructionwebapp/ConstructionWebappApplication.java
@@ -0,0 +1,13 @@
+package gr.pf.team2.constructionwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/pf/team2/constructionwebapp/ConstructionWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.pf.team2.constructionwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
