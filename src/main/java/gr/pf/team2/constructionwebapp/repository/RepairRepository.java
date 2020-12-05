face8fbbaec26c2ae2b031c5126fe968abbf160a
Petros <petros.1822@gmail.com>
2020-12-05 15:26:36 +0200

    *False*Test Controller for Owner

--- src/main/java/gr/pf/team2/constructionwebapp/repository/RepairRepository.java
+++ src/main/java/gr/pf/team2/constructionwebapp/repository/RepairRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepairRepositiry extends JpaRepository<Repair,Long> {
+public interface RepairRepository extends JpaRepository<Repair,Long> {
 }
