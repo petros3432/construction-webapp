9169609e11e9ed25c222fb2c70b013e76a35fb83
Petros <petros.1822@gmail.com>
2020-12-05 15:26:36 +0200

    Test Controller for Owner

--- src/main/java/gr/pf/team2/constructionwebapp/service/RepairServiceImpl.java
+++ src/main/java/gr/pf/team2/constructionwebapp/service/RepairServiceImpl.java
@@ -1,13 +1,13 @@
 package gr.pf.team2.constructionwebapp.service;
 
 
-import gr.pf.team2.constructionwebapp.repository.RepairRepositiry;
+import gr.pf.team2.constructionwebapp.repository.RepairRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RepairServiceImpl implements RepairService {
     @Autowired
-    private RepairRepositiry repairRepository;
+    private RepairRepository repairRepository;
 
 }
