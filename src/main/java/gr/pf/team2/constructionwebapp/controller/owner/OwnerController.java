d1728c489b348e6a23d41e1981cb2d9e77fc869e
Petros <petros.1822@gmail.com>
2020-12-16 13:18:06 +0200

    Owner's Home Page Created

--- src/main/java/gr/pf/team2/constructionwebapp/controller/owner/OwnerController.java
+++ src/main/java/gr/pf/team2/constructionwebapp/controller/owner/OwnerController.java
@@ -2,6 +2,7 @@ package gr.pf.team2.constructionwebapp.controller.owner;
 
 
 import gr.pf.team2.constructionwebapp.domain.Owner;
+import gr.pf.team2.constructionwebapp.models.OwnerModel;
 import gr.pf.team2.constructionwebapp.service.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,27 +19,29 @@ public class OwnerController {
     @Autowired
     private OwnerService ownerService;
 
+    private static final String OWNERS_REPAIRS = "repairs";
 
-    /*@GetMapping({"ownerByAfm/{afm}"})
+
+    @GetMapping({"/owner_user/{afm}"})
     public String OwnerByAfm(Model model,@PathVariable String afm) {
-        Owner owner = ownerService.findOwnerByAfm(afm).orElseThrow();
-        model.addAttribute("owner", owner);
-        return "pages/AdminHomePage";
+//        OwnerModel ownerModel = ownerService.findOwnerById(id);
+        Owner owner = ownerService.findOwnerByAfmOwner(afm).orElseThrow();
+        model.addAttribute(OWNERS_REPAIRS, owner.getRepairs());
+        return "pages/OwnerHomePage";
     }
 
-    @GetMapping({"ownerByEmail/{email}"})
-    public String OwnerByEmail(Model model,@PathVariable String email) {
-        Owner owner = ownerService.findOwnerByEmail(email).orElseThrow();
-        model.addAttribute("owner", owner);
-        return "pages/AdminHomePage";
-    }
-
-    @GetMapping({"ownerUpdate/{id}"})
-    public String UpdateOwnerProperty(Model model,@ModelAttribute Owner owner) {
-        Owner databaseOwner = ownerService.findOwnerById(owner.getId()).orElseThrow();
-        databaseOwner = ownerService.updateOwner(owner,databaseOwner);
-        model.addAttribute("owner", databaseOwner);
-        return "pages/AdminHomePage";
-    }
-    */
+//    @GetMapping({"owner_user/{email}"})
+//    public String OwnerByEmail(Model model,@PathVariable String email) {
+//        Owner owner = ownerService.findOwnerByEmail(email).orElseThrow();
+//        model.addAttribute("owner", owner);
+//        return "pages/OwnerHomePage";
+//    }
+//
+//    @GetMapping({"owner_user/{id}"})
+//    public String UpdateOwnerProperty(Model model,@ModelAttribute Owner owner) {
+//        Owner databaseOwner = ownerService.findOwnerById(owner.getId()).orElseThrow();
+//        databaseOwner = ownerService.updateOwner(owner,databaseOwner);
+//        model.addAttribute("owner", databaseOwner);
+//        return "pages/OwnerHomePage";
+//    }
 }
